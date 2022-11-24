10bcb669593bbae33e3212d2e6b2fb31f571a5cb
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-11-24 23:56:40 +0100

    Update

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,15 +20,28 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
